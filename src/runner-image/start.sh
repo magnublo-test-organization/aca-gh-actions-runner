17f902cca4fc44d9562a9269c0c37122dae83350
Magnus Longva <magnus@longva.email>
2024-01-25 10:13:34 +0100

    start.sh in base version of image

--- src/runner-image/start.sh
+++ src/runner-image/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 access_token=$(cat $TOKEN_FILE)
-echo "access_token: $access_token" #debug
-echo "env variables: $(printenv)" #debug
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${access_token}" https://api.github.com/orgs/"${ORG_NAME}"/actions/runners/registration-token | jq .token --raw-output)
 ./config.sh --url https://github.com/"${ORG_NAME}" --token "${REG_TOKEN}" --unattended "${RUNNER_LABELS}" --ephemeral && ./run.sh
